07079fd6b92355f90ea518ca1542f094b5fd3186
jarne <jarne@LAPTOP-40O2DF4T>
2018-09-21 14:19:53 +0200

    added custom simulator banner.

--- simulator/src/main/java/be/kdg/simulator/SimulatorApplication.java
+++ simulator/src/main/java/be/kdg/simulator/SimulatorApplication.java
@@ -3,6 +3,9 @@ package be.kdg.simulator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//cyclic dependency
+// => gebruik bij de ene constructor
+// => gebruik field injection bij de andere
 @SpringBootApplication
 public class SimulatorApplication {
     public static void main(String[] args) {
--- simulator/src/main/java/be/kdg/simulator/messengers/CommandLineMessenger.java
+++ simulator/src/main/java/be/kdg/simulator/messengers/CommandLineMessenger.java
@@ -1,6 +1,7 @@
 package be.kdg.simulator.messengers;
 
 import be.kdg.simulator.generators.MessageGenerator;
+import com.fasterxml.jackson.annotation.JacksonInject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
