0d62936c01e9e6eadae2be42599bf35be2dcda11
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-01 20:51:55 +0200

    Enabled scheduling

--- processor/src/main/java/be/kdg/processor/config/GlobalConfig.java
+++ processor/src/main/java/be/kdg/processor/config/GlobalConfig.java
@@ -1,7 +1,9 @@
 package be.kdg.processor.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 public class GlobalConfig {
 }
