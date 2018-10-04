0ee555721b8edee511dc3d0c784ab8bb11c96b9e
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-04 14:36:50 +0200

    Deleted JSONService

--- processor/src/main/java/be/kdg/processor/services/SpeedfineDetectionService.java
+++ processor/src/main/java/be/kdg/processor/services/SpeedfineDetectionService.java
@@ -1,12 +1,31 @@
 package be.kdg.processor.services;
 
 import be.kdg.processor.models.cameras.CameraMessage;
+import be.kdg.sa.services.CameraServiceProxy;
+import be.kdg.sa.services.LicensePlateServiceProxy;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SpeedfineDetectionService implements DetectionService<CameraMessage> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmmissonDetectionService.class);
+    private final CameraServiceProxy cameraServiceProxy;
+    private final LicensePlateServiceProxy licensePlateServiceProxy;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public SpeedfineDetectionService(CameraServiceProxy cameraServiceProxy, LicensePlateServiceProxy licensePlateServiceProxy, ObjectMapper objectMapper) {
+        this.cameraServiceProxy = cameraServiceProxy;
+        this.licensePlateServiceProxy = licensePlateServiceProxy;
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void detectFine(CameraMessage message) {
-        //TODO: implement detection logic.
+        System.out.println("test");
+       //TODO: detect speedfine
     }
 }
