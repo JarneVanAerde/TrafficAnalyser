4a43f4a6b6469b17cbb3fdc9b07d74847f6d674d
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-05 12:06:44 +0200

    speed detection service

--- processor/src/main/java/be/kdg/processor/services/SpeedfineDetectionService.java
+++ processor/src/main/java/be/kdg/processor/services/SpeedfineDetectionService.java
@@ -1,13 +1,16 @@
 package be.kdg.processor.services;
 
+import be.kdg.processor.models.cameras.Camera;
 import be.kdg.processor.models.cameras.CameraMessage;
-import be.kdg.sa.services.CameraServiceProxy;
-import be.kdg.sa.services.LicensePlateServiceProxy;
+import be.kdg.processor.models.licensePlates.LicensePlateInfo;
+import be.kdg.sa.services.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class SpeedfineDetectionService implements DetectionService<CameraMessage> {
@@ -15,16 +18,35 @@ public class SpeedfineDetectionService implements DetectionService<CameraMessage
     private final CameraServiceProxy cameraServiceProxy;
     private final LicensePlateServiceProxy licensePlateServiceProxy;
     private final ObjectMapper objectMapper;
+    private final FineService fineService;
 
     @Autowired
-    public SpeedfineDetectionService(CameraServiceProxy cameraServiceProxy, LicensePlateServiceProxy licensePlateServiceProxy, ObjectMapper objectMapper) {
+    public SpeedfineDetectionService(CameraServiceProxy cameraServiceProxy, LicensePlateServiceProxy licensePlateServiceProxy, ObjectMapper objectMapper, FineService fineService) {
         this.cameraServiceProxy = cameraServiceProxy;
         this.licensePlateServiceProxy = licensePlateServiceProxy;
         this.objectMapper = objectMapper;
+        this.fineService = fineService;
     }
 
     @Override
-    public void detectFine(CameraMessage message) {
-       //TODO: detect speedfine
+    public void detectFine(CameraMessage message) throws IOException, LicensePlateNotFoundException, CameraNotFoundException, InvalidLicensePlateException {
+        //Extract JSON
+        String cameraJson = cameraServiceProxy.get(message.getId());
+        String licenseJson = licensePlateServiceProxy.get(message.getLicensePlate());
+        Camera camera = objectMapper.readValue(cameraJson, Camera.class);
+        LicensePlateInfo licensePlateInfo = objectMapper.readValue(licenseJson, LicensePlateInfo.class);
+
+        //Detect fine
+        //TODO: detect speedfine
+    }
+
+    private double calculateFine() {
+        //TODO: calculate fine
+        return 1000.0;
+    }
+
+    private double calculateSpeed() {
+        //TODO: calculate speed
+        return 100.0;
     }
 }
