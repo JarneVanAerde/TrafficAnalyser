2ffea9864a152362fa6d7d71d361f0c22d69e912
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-05 19:13:20 +0200

    Adapter implemented

---
+++ processor/src/main/java/be/kdg/processor/services/impl/CameraInfoService.java
@@ -0,0 +1,29 @@
+package be.kdg.processor.services.impl;
+
+import be.kdg.processor.models.cameras.Camera;
+import be.kdg.processor.services.api.CameraServiceAdapter;
+import be.kdg.sa.services.CameraNotFoundException;
+import be.kdg.sa.services.CameraServiceProxy;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class CameraInfoService implements CameraServiceAdapter {
+    private final CameraServiceProxy cameraServiceProxy;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public CameraInfoService(CameraServiceProxy cameraServiceProxy, ObjectMapper objectMapper) {
+        this.cameraServiceProxy = cameraServiceProxy;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Camera get(int id) throws IOException, CameraNotFoundException {
+        String cameraJson = cameraServiceProxy.get(id);
+        return objectMapper.readValue(cameraJson, Camera.class);
+    }
+}
