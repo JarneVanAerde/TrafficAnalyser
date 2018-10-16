2b40d4388afa366f4936c8b98a6fdbc86169b1c7
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-16 20:15:47 +0200

    Added user api controller

--- processor/src/main/java/be/kdg/processor/presentation/controllers/api/UserApiController.java
+++ processor/src/main/java/be/kdg/processor/presentation/controllers/api/UserApiController.java
@@ -1,9 +1,60 @@
 package be.kdg.processor.presentation.controllers.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import be.kdg.processor.models.users.User;
+import be.kdg.processor.presentation.dto.UserDTO;
+import be.kdg.processor.services.exceptions.ServiceException;
+import be.kdg.processor.services.impl.modelservices.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class UserApiController {
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserApiController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/users/active")
+    public ResponseEntity<UserDTO[]> loadUsers() {
+        List<User> users = userService.getUsers(false);
+        return new ResponseEntity<>(modelMapper.map(users, UserDTO[].class), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDTO> loadUser(@PathVariable int id) throws ServiceException {
+        User user = userService.getUser(id);
+        return new ResponseEntity<>(modelMapper.map(user, UserDTO.class), HttpStatus.OK);
+    }
+
+    @PostMapping("/createUser")
+    public ResponseEntity<UserDTO> createUser(@RequestBody @Valid UserDTO userDTO) {
+        return saveUser(userDTO);
+    }
+
+    @PostMapping("/updateUser")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody @Valid UserDTO userDTO) {
+        return saveUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public ResponseEntity<UserDTO> deleteUser(@PathVariable int id) throws ServiceException {
+        User userToDelete = userService.deleteUser(id);
+        return new ResponseEntity<>(modelMapper.map(userToDelete, UserDTO.class), HttpStatus.CREATED);
+    }
+
+    private ResponseEntity<UserDTO> saveUser(UserDTO userDTO) {
+        User createdUser = userService.saveUser(modelMapper.map(userDTO, User.class));
+        return new ResponseEntity<>(modelMapper.map(createdUser, UserDTO.class), HttpStatus.CREATED);
+    }
 }
--- processor/src/main/java/be/kdg/processor/services/impl/modelservices/UserService.java
+++ processor/src/main/java/be/kdg/processor/services/impl/modelservices/UserService.java
@@ -2,7 +2,7 @@ package be.kdg.processor.services.impl.modelservices;
 
 import be.kdg.processor.models.users.User;
 import be.kdg.processor.persistence.UserRepository;
-import org.hibernate.service.spi.ServiceException;
+import be.kdg.processor.services.exceptions.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,11 +29,11 @@ public class UserService {
 
     public User getUser(int id) throws ServiceException {
         return userRepository.findById(id)
-                .filter(user -> !user.isDeleted())
+                //.filter(user -> !user.isDeleted())
                 .orElseThrow(() -> new ServiceException(getClass().getSimpleName() + ": user with id " + id + " wasn't found in the database"));
     }
 
-    public User deleteUser(int id) {
+    public User deleteUser(int id) throws ServiceException {
         User userToDelete = getUser(id);
         userToDelete.setDeleted(true);
         return saveUser(userToDelete);
