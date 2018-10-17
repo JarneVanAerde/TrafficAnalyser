a3a4e4249aee993b278548f60ab6e3ecd96b50c0
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-17 15:51:23 +0200

    Wrote test for api

---
+++ processor/src/test/java/be/kdg/processor/presentation/controllers/api/FineApiControllerTest.java
@@ -0,0 +1,34 @@
+package be.kdg.processor.presentation.controllers.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FineApiControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void loadFinesEmpty() throws Exception {
+        mockMvc.perform(get("/api/fines")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isNoContent());
+    }
+}
