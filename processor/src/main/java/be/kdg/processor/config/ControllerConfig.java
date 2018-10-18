51331470413148beb9d579980f960047396a8a5b
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-18 22:53:38 +0200

    Cleaned up config classes, added documentation

--- processor/src/main/java/be/kdg/processor/config/ControllerConfig.java
+++ processor/src/main/java/be/kdg/processor/config/ControllerConfig.java
@@ -4,6 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * This config class is used for the configuration of a model mapper.
+ * A model mapper is needed to map a model to a DTO.
+ */
 @Configuration
 public class ControllerConfig {
     @Bean
