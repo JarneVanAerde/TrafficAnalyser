f293758cdb002c0715cd09cf42d500d7c4ddd8fb
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-18 14:51:19 +0200

    Trying to fix css

---
+++ processor/src/main/java/be/kdg/processor/presentation/dto/OptionsDTO.java
@@ -0,0 +1,16 @@
+package be.kdg.processor.presentation.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OptionsDTO {
+    private List<OptionDTO> optionDTOS;
+
+    public OptionsDTO() {
+        optionDTOS = new ArrayList<>();
+    }
+
+    public void addOption(OptionDTO optionDTO) {
+
+    }
+}
