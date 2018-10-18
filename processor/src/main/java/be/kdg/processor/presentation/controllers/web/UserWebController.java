eccd5d8ba52daf2cd1236c8d11638eb1eae0e5b3
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-18 16:24:12 +0200

    trying to create users using login

--- processor/src/main/java/be/kdg/processor/presentation/controllers/web/UserWebController.java
+++ processor/src/main/java/be/kdg/processor/presentation/controllers/web/UserWebController.java
@@ -1,25 +1,43 @@
 package be.kdg.processor.presentation.controllers.web;
 
+import be.kdg.processor.models.users.User;
 import be.kdg.processor.presentation.dto.UserDTO;
 import be.kdg.processor.services.impl.modelservices.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/user")
 public class UserWebController {
     private final UserService userService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public UserWebController(UserService userService) {
+    public UserWebController(UserService userService, ModelMapper modelMapper) {
         this.userService = userService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping("/login")
     public ModelAndView showGreetingsForm(UserDTO userDTO) {
         return new ModelAndView("login", "userDTO", userDTO);
+    }
+
+    @GetMapping("/")
+    public ModelAndView showGreetingsForm() {
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/menu")
+    public ModelAndView showMenu(@Valid UserDTO userDTO) {
+        User user = modelMapper.map(userDTO, User.class);
+        userService.makeNewUserIfNeeded(user);
+        return null;
     }
 }
