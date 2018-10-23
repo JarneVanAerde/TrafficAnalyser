31869ea9ae391ffe3b20090c2d0735ad0a86d758
jarne <jarne@LAPTOP-40O2DF4T>
2018-10-23 10:50:16 +0200

    cleaning up code

--- processor/src/main/java/be/kdg/processor/config/QueueConfig.java
+++ processor/src/main/java/be/kdg/processor/config/QueueConfig.java
@@ -3,6 +3,10 @@ package be.kdg.processor.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * configuration that is used to pause and stop the queue
+ * based on the id that comes from applicaton.properties
+ */
 @Configuration
 public class QueueConfig {
     @Value("${queue.id}")
--- processor/src/main/java/be/kdg/processor/presentation/controllers/api/UserApiController.java
+++ processor/src/main/java/be/kdg/processor/presentation/controllers/api/UserApiController.java
@@ -13,6 +13,10 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * This api controller is used for
+ * CRUD REST class thar are related to users.
+ */
 @RestController
 @RequestMapping("/api")
 public class UserApiController {
@@ -25,24 +29,42 @@ public class UserApiController {
         this.modelMapper = modelMapper;
     }
 
+    /**
+     * @return get all the active users of the system
+     */
     @GetMapping("/users/active")
     public ResponseEntity<UserDTO[]> loadUsers() {
         List<User> users = userService.getUsers(false);
         return new ResponseEntity<>(modelMapper.map(users, UserDTO[].class), HttpStatus.OK);
     }
 
+    /**
+     * @param id user id
+     * @return requested user
+     * @throws ServiceException wrapper-exception
+     */
     @GetMapping("/users/{id}")
     public ResponseEntity<UserDTO> loadUser(@PathVariable int id) throws ServiceException {
         User user = userService.getUser(id);
         return new ResponseEntity<>(modelMapper.map(user, UserDTO.class), HttpStatus.OK);
     }
 
+    /**
+     * @param userDTO user that needs to be crated
+     * @return created user
+     * @throws ServiceException wrapper-exception
+     */
     @PostMapping("/users/save")
     public ResponseEntity<UserDTO> createUser(@RequestBody @Valid UserDTO userDTO) throws ServiceException {
         User createdUser = userService.saveUser(modelMapper.map(userDTO, User.class));
         return new ResponseEntity<>(modelMapper.map(createdUser, UserDTO.class), HttpStatus.CREATED);
     }
 
+    /**
+     * @param id the id of the user that needs to be deleted
+     * @return the deleted user
+     * @throws ServiceException wrapper-exception
+     */
     @DeleteMapping("/users/delete/{id}")
     public ResponseEntity<UserDTO> deleteUser(@PathVariable int id) throws ServiceException {
         User userToDelete = userService.deleteUser(id);
