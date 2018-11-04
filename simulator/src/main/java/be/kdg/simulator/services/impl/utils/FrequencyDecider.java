1475cc89a5f7dcc090d1d2db9c3e63acd34a5a27
jarne <jarne@LAPTOP-40O2DF4T>
2018-11-04 17:46:11 +0100

    Fixed typos

--- simulator/src/main/java/be/kdg/simulator/services/impl/utils/FrequencyDecider.java
+++ simulator/src/main/java/be/kdg/simulator/services/impl/utils/FrequencyDecider.java
@@ -12,10 +12,10 @@ import java.time.format.DateTimeFormatter;
 @Service
 public class FrequencyDecider {
     private final FrequencyConfig frequencyConfig;
-    private LocalTime timeMoringBegin;
-    private LocalTime timeMoringEnd;
+    private LocalTime timeMorningBegin;
+    private LocalTime timeMorningEnd;
     private LocalTime timeEveningBegin;
-    private LocalTime timeEvenigEnd;
+    private LocalTime timeEveningEnd;
 
     public FrequencyDecider(FrequencyConfig frequencyConfig) {
         this.frequencyConfig = frequencyConfig;
@@ -30,12 +30,12 @@ public class FrequencyDecider {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
         String[] morningHours = frequencyConfig.getMorningHours().split("-");
-        timeMoringBegin = LocalTime.parse(morningHours[0], formatter);
-        timeMoringEnd = LocalTime.parse(morningHours[1], formatter);
+        timeMorningBegin = LocalTime.parse(morningHours[0], formatter);
+        timeMorningEnd = LocalTime.parse(morningHours[1], formatter);
 
         String[] eveningHours = frequencyConfig.getEveningHours().split("-");
         timeEveningBegin = LocalTime.parse(eveningHours[0], formatter);
-        timeEvenigEnd = LocalTime.parse(eveningHours[1], formatter);
+        timeEveningEnd = LocalTime.parse(eveningHours[1], formatter);
     }
 
     /**
@@ -45,8 +45,8 @@ public class FrequencyDecider {
     public long getFrequency() {
         LocalTime now = LocalTime.now();
 
-        if ((now.isAfter(timeMoringBegin) && now.isBefore(timeMoringEnd)
-                || (now.isAfter(timeEveningBegin) && now.isBefore(timeEvenigEnd)))) return frequencyConfig.getPeakRate();
+        if ((now.isAfter(timeMorningBegin) && now.isBefore(timeMorningEnd)
+                || (now.isAfter(timeEveningBegin) && now.isBefore(timeEveningEnd)))) return frequencyConfig.getPeakRate();
         else return frequencyConfig.getNormalRate();
     }
 }
